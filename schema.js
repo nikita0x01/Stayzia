2f233be46597c2f7d921f602ec7552a8dad72f83
Nikita Satpute <satputenikita204@gmail.com>
2026-02-01 17:29:51 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,6 +1,5 @@
 const Joi = require('joi');
 const review = require('./models/review');
-
 module.exports.listingSchema = Joi.object({
   listing :Joi.object({
     title : Joi.string().required(),
@@ -11,7 +10,6 @@ module.exports.listingSchema = Joi.object({
     image : Joi.string().allow("", null )
   }).required(),
 });
-
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
     rating: Joi.number().required().min(1).max(5),

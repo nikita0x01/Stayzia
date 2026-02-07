36f1616db9b00de688b9618ae684b5a3e428154f
Nikita Satpute <satputenikita204@gmail.com>
2026-02-07 17:10:47 +0530

    Update listing.js

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,3 +1,4 @@
+//business logic of listing
 const Listing = require("../models/listing");
 module.exports.index = async (req, res) => {
   let allListings = await Listing.find({});
@@ -6,6 +7,7 @@ module.exports.index = async (req, res) => {
 module.exports.renderNewForm = (req , res ) => (req, res) => {
   res.render("listings/new"); //  render the correct EJS template
 };
+//show listings
 module.exports.showListing = async (req, res) => {
   const listing = await Listing.findById(req.params.id).populate({ path: "reviews" , populate:{path:"author"},}).populate("owner");
   if(!listing) {
@@ -16,7 +18,7 @@ module.exports.showListing = async (req, res) => {
   res.render("listings/show", { listing });
 };
 
-
+//create listing
 module.exports.createListing = async (req, res) => {
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
@@ -24,18 +26,22 @@ module.exports.createListing = async (req, res) => {
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 };
+
+//edit listing
 module.exports.editListing= async (req, res) => {
   const listing = await Listing.findById(req.params.id);
   
   res.render("listings/edit", { listing });
 };
 
+//update listing
 module.exports.updateListing = async (req, res) => {
   await Listing.findByIdAndUpdate(req.params.id, { ...req.body.listing });
   req.flash("success", "Listing Updated Successfully!");
   res.redirect(`/listings/${req.params.id}`);
 };
 
+//delete listing
 module.exports.deleteListing = async (req, res) => {
   await Listing.findByIdAndDelete(req.params.id);
   req.flash("success", " Listing Deleted!");
